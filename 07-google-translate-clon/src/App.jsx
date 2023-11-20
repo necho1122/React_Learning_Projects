5f2c5cd77756926e41cbc6562a01df1e9b79e274
Nelson Ubac <nelsonubac90@gmail.com>
2023-11-20 17:05:12 -0300

    icons update

--- 07-google-translate-clon/src/App.jsx
+++ 07-google-translate-clon/src/App.jsx
@@ -1,19 +1,38 @@
 import { useState } from 'react';
 import translateText from './logic/translateText.js';
 import translateIcon from '../public/translate-icon.svg';
+import switchButton from '../public/switch-horizontal.svg';
+import { languages } from './services/languages.js';
 import './App.css';
 
 function App() {
 	const [translatedText, setTranslatedText] = useState('');
 
+	
+
 	const handleTranslation = async () => {
 		const text = document.querySelector('input').value;
-		const fromLanguage = 'auto';
-		const toLanguage = 'en';
+		const fromLanguage = document.querySelector('#from').value;
+		const toLanguage = document.querySelector('#to').value;
 
 		const translatedText = await translateText(text, fromLanguage, toLanguage);
 		setTranslatedText(translatedText);
 	};
+
+	const handleSwitch = () => {
+		const fromLanguage = document.querySelector('#from').value;
+		const toLanguage = document.querySelector('#to').value;
+
+		document.querySelector('#from').value = toLanguage;
+		document.querySelector('#to').value = fromLanguage;
+
+		const input = document.querySelector('input').value;
+		document.querySelector('input').value = translatedText;
+
+		setTranslatedText(input);
+
+		handleTranslation();
+	}
 
 	return (
 		<div className='container'>
@@ -45,10 +64,33 @@ function App() {
           </li>
 				</ul>
 			</div>
+			<div className="languages-container">
+			<div className="input-languages"></div>
+			<div className="language-selector">
+				<select name="from" id="from" onChange={handleTranslation}>
+					{languages.map((language) => (
+						<option value={language.code} key={language.code}>
+							{language.name}
+						</option>
+					))}
+				</select>
+				<button onClick={handleSwitch}><img src={switchButton} alt="switch button" /></button>
+				<select name="to" id="to" onChange={handleTranslation}>
+					{languages.map((language) => (
+						<option value={language.code} key={language.code} disabled={language.name === 'Auto'}>
+							{
+								language.name === 'Auto' ? 'English' : language.name
+							}
+						</option>
+					))}
+				</select>
+				<div className="output-languages"></div>
+			</div>
+			</div>
 			<div className='translate-body'>
 				<input
 					type='text'
-					placeholder='escribe algo'
+					placeholder='Escribe algo'
 					onChange={handleTranslation}
 				/>
 				<p className='output'>{translatedText}</p>
---
+++ 07-google-translate-clon/src/services/languages.js
@@ -0,0 +1,239 @@
+export const languages = [
+    {
+        name: 'Auto',
+        code: 'auto',
+    },
+    {
+        name: 'English',
+        code: 'en',
+    },
+    {
+        name: 'French',
+        code: 'fr',
+    },
+    {
+        name: 'Spanish',
+        code: 'es',
+    },
+    {
+        name: 'German',
+        code: 'de',
+    },
+    {
+        name: 'Italian',
+        code: 'it',
+    },
+    {
+        name: 'Russian',
+        code: 'ru',
+    },
+    {
+        name: 'Japanese',
+        code: 'ja',
+    },
+    {
+        name: 'Korean',
+        code: 'ko',
+    },
+    {
+        name: 'Chinese',
+        code: 'zh',
+    },
+    {
+        name: 'Arabic',
+        code: 'ar',
+    },
+    {
+        name: 'Hindi',
+        code: 'hi',
+    },
+    {
+        name: 'Indonesian',
+        code: 'id',
+    },
+    {
+        name: 'Turkish',
+        code: 'tr',
+    },
+    {
+        name: 'Portuguese',
+        code: 'pt',
+    },
+    {
+        name: 'Vietnamese',
+        code: 'vi',
+    },
+    {
+        name: 'Thai',
+        code: 'th',
+    },
+    {
+        name: 'Polish',
+        code: 'pl',
+    },
+    {
+        name: 'Dutch',
+        code: 'nl',
+    },
+    {
+        name: 'Czech',
+        code: 'cs',
+    },
+    {
+        name: 'Swedish',
+        code: 'sv',
+    },
+    {
+        name: 'Greek',
+        code: 'el',
+    },
+    {
+        name: 'Finnish',
+        code: 'fi',
+    },
+    {
+        name: 'Danish',
+        code: 'da',
+    },
+    {
+        name: 'Norwegian',
+        code: 'no',
+    },
+    {
+        name: 'Hungarian',
+        code: 'hu',
+    },
+    {
+        name: 'Romanian',
+        code: 'ro',
+    },
+    {
+        name: 'Ukrainian',
+        code: 'uk',
+    },
+    {
+        name: 'Catalan',
+        code: 'ca',
+    },
+    {
+        name: 'Croatian',
+        code: 'hr',
+    },
+    {
+        name: 'Hebrew',
+        code: 'he',
+    },
+    {
+        name: 'Farsi',
+        code: 'fa',
+    },
+    {
+        name: 'Malay',
+        code: 'ms',
+    },
+    {
+        name: 'Urdu',
+        code: 'ur',
+    },
+    {
+        name: 'Estonian',
+        code: 'et',
+    },
+    {
+        name: 'Lithuanian',
+        code: 'lt',
+    },
+    {
+        name: 'Slovak',
+        code: 'sk',
+    },
+    {
+        name: 'Slovenian',
+        code: 'sl',
+    },
+    {
+        name: 'Bulgarian',
+        code: 'bg',
+    },
+    {
+        name: 'Latvian',
+        code: 'lv',
+    },
+    {
+        name: 'Albanian',
+        code: 'sq',
+    },
+    {
+        name: 'Serbian',
+        code: 'sr',
+    },
+    {
+        name: 'Georgian',
+        code: 'ka',
+    },
+    {
+        name: 'Armenian',
+        code: 'hy',
+    },
+    {
+        name: 'Macedonian',
+        code: 'mk',
+    },
+    {
+        name: 'Afrikaans',
+        code: 'af',
+    },
+    {
+        name: 'Basque',
+        code: 'eu',
+    },
+    {
+        name: 'Esperanto',
+        code: 'eo',
+    },
+    {
+        name: 'Latin',
+        code: 'la',
+    },
+    {
+        name: 'Swahili',
+        code: 'sw',
+    },
+    {
+        name: 'Tagalog',
+        code: 'tl',
+    },
+    {
+        name: 'Yiddish',
+        code: 'yi',
+    },
+    {
+        name: 'Welsh',
+        code: 'cy',
+    },
+    {
+        name: 'Icelandic',
+        code: 'is',
+    },
+    {
+        name: 'Azerbaijani',
+        code: 'az',
+    },
+    {
+        name: 'Mongolian',
+        code: 'mn',
+    },
+    {
+        name: 'Bengali',
+        code: 'bn',
+    },
+    {
+        name: 'Punjabi',
+        code: 'pa',
+    },
+    {
+        name: 'Gujarati',
+        code: 'gu',
+    },
+
+];
